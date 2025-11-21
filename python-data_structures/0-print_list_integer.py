b620648fa007489fa4a52fb58a8fad3d9142b399
Admahmudluzulaya <mahmudovazulaye@gmail.com>
2025-11-21 08:27:20 +0000

    helloff

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,20 +2,12 @@
 """
 Module: 0-print_list_integer
 Prints all integers in a list, one per line, using str.format()
-Raises TypeError if a non-integer is found
 """
-
-
 def print_list_integer(my_list=[]):
     """
-    Prints all integers in my_list using str.format()
-
+    Prints all integers in my_list using str.format() with {:d}
     Args:
         my_list (list): List of integers
-    Raises:
-        TypeError: if any element in the list is not an integer
     """
     for i in my_list:
-        if not isinstance(i, int):
-            raise TypeError("All elements must be integers")
-        print("{}".format(i))
+        print("{:d}".format(i))
