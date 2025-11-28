1790f0c26865a4fb19a5883b9e15b126d20789e9
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-11-28 09:54:57 +0000

    hbf

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Module that contains the write_file function."""
+
+
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF-8) and returns the number of characters written."""
+    """Writes a string to a text file (UTF-8) and returns
+    the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
