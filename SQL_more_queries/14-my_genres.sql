0d77cd4c15709a398312d34838a24e43ad76951c
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-11-28 14:17:08 +0000

    ruu

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,12 @@
+-- List all genres for the show "Dexter"
+-- Must use only one SELECT statement
+-- Results sorted by genre name (ASC)
+
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
