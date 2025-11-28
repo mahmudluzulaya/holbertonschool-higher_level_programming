070f878e6c9b533798c2963e4c16f65c449a3f8c
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-11-28 13:35:51 +0000

    fjf

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- This script displays all privileges for the MySQL users
+-- user_0d_1 and user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
