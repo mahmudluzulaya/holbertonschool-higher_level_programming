9fed000e916a47ad2710ff9d9850103f5d0aaaac
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-11-28 11:15:24 +0000

    hjdfkmx

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list and saves them to a JSON file."""
+"""Script that adds all arguments to a Python list and
+saves them to a JSON file.
+"""
 
 import sys
 from pathlib import Path
