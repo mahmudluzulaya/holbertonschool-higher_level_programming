48844fe7a3b63131140044d9aafd82cb694497c9
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-11-28 09:49:38 +0000

    Add shebang to 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def write_file(filename="", text=""):
 """Writes a string to a text file (UTF-8) and returns the number of characters written."""
 with open(filename, "w", encoding="utf-8") as f:
