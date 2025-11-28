5cb555c85c9f47d15039a5d94a052ad429af945c
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-11-28 14:09:47 +0000

    ffk

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,9 @@
--- List all shows with at least one genre linked
--- Display: tv_shows.title - tv_show_genres.genre_id
--- Sorted by tv_shows.title ASC, then genre_id ASC
+-- List all shows that have at least one genre linked
+-- Output: tv_shows.title - tv_show_genres.genre_id
+-- Only one SELECT statement allowed
+-- Results sorted by title ASC, then genre_id ASC
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
