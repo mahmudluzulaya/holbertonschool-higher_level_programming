b0604b43189b80a215a90bcb88f050d53d6aa390
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-11-28 14:04:02 +0000

    ffk

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- List all shows that have at least one genre
+-- Output: tv_shows.title - tv_show_genres.genre_id
+-- Sorted by tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
