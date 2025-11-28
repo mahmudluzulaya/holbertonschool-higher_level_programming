6ad5d53e5bab6c885de3903ad371d64f9eac66b3
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-11-28 10:04:31 +0000

    hdbk

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that contains the from_json_string function."""
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns the Python object represented by a JSON string."""
+    return json.loads(my_str)
