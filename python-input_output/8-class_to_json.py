0c7077d35e716bfb119b5744f1cf8e2dc6bc69d8
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-11-28 11:17:39 +0000

    kjdhn

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that returns the dictionary description for JSON serialization of an object."""
+
+def class_to_json(obj):
+    """
+    Returns the dictionary representation of a class instance
+    with only serializable attributes (list, dict, str, int, bool).
+    """
+    return obj.__dict__.copy()
