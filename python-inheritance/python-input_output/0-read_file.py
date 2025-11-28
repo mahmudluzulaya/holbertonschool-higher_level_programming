57c8b6c19e50bb9e45a259e34a2cdf83880bd2e7
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-11-28 09:11:55 +0000

    bdj

---
+++ python-inheritance/python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Reads a UTF-8 text file and prints its content"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
