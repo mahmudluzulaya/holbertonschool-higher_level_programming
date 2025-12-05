70c6f139e616e8429719807dff2d6b95b8b3fb4a
mahmudluzulaya <mahmudovazulaye@gmail.com>
2025-12-05 10:33:30 +0000

    jffnk

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Search API that sends a POST request with a letter and handles JSON response."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {"q": q}
+
+    try:
+        response = requests.post(url, data=payload)
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
